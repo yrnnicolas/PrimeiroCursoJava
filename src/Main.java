769616c7434df668f1496d70a264a84b44d4a372
Nicolas Fagundes <nickmendoncafagundes@gmail.com>
2026-06-17 10:35:22 -0300

    Adição e Variaveis

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!\n");
+        System.out.println("Tudo Bem?");
+
+        int resultado = somar(10, 5);
+        System.out.println("A soma é: " + resultado);
+
+        System.out.println("10 - 5");
+    }
+
+    public static int somar(int a, int b) {
+        return a + b;
+
+        
     }
 }
