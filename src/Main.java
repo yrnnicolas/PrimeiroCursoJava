be710e08a0efc8cd9b0e2e988436232cc8af5529
Nicolas Fagundes <nickmendoncafagundes@gmail.com>
2026-06-17 12:08:24 -0300

    Classificacao e conversão

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,13 @@ public class Main {
         double notaDoFilme = 8.1;
         String sinopse;
         sinopse = """
-                Top Gun: Maverick é um filme de ação e aventura lançado em %s, 
+                Top Gun: Maverick é um filme de ação e aventura lançado em %d, 
                 dirigido por Joseph Kosinski. O filme é uma sequência do clássico de 1986, Top Gun
                  """.formatted(anoDeLancamento);
         System.out.println("Sinopse: " + sinopse);
+
+        double media = (9.8 + 6.2 + 8.0) /3;
+        int classificacao = (int) (media / 2);
+        System.out.println("Classificação: " + classificacao);
     }
 }
